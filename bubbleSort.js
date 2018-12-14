833567ca47ae7f0c80d476cb28d4e9fb98b304be
Sebastian <sebgertz@gmail.com>
2018-12-14 13:27:18 -0500

    simple implementation of bubble

---
+++ bubbleSort.js
@@ -0,0 +1,15 @@
+function bubbleSort(nums) {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > nums[i+1]) {
+        const temp = nums[i];
+        nums[i] = nums[i+1];
+        nums[i+1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped)
+  return nums
+}
