823dade95d810d1bd1df476808c0d9f94cda7247
Sebastian <sebgertz@gmail.com>
2019-02-04 15:45:47 -0500

    add alternative solution

--- capitalize.js
+++ capitalize.js
@@ -1,5 +1,6 @@
 // write a function that capitalizes the first letter of every word in a string
 
+// solution 1: split string into words and capitalize first letter of each one
 function capitalize(str) {
   let result = [];
   for (let word of str.split(" ")) {
@@ -9,3 +10,16 @@ function capitalize(str) {
 }
 
 capitalize("a lazy fox")
+
+// solution 2: capitalize first letter of string, and then any subsequent letter that follows a space
+function capitalize(str) {
+  let result =  str[0].toUpperCase();
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase()
+    } else {
+      result += str[i]
+    }
+  }
+  return result
+}
