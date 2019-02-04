2b907fded01831d92e8b2b65c67a7f8198e38fd4
Sebastian <sebgertz@gmail.com>
2019-02-03 21:44:46 -0500

    add alternative solution to array chunk

--- arrayChunk.js
+++ arrayChunk.js
@@ -1,7 +1,7 @@
 // write a function that chunks a given array into as many subarrays ("chunks") of n lengths as possible
 
 function arrayChunk(arr, n) {
-  let result = [];
+  const result = [];
   let counter = arr.length;
   while (counter > 0) {
     result.push(arr.slice(0,n))
@@ -9,6 +9,20 @@ function arrayChunk(arr, n) {
     arr = arr.slice(n)
   }
   return result
+}
+
+// alternative solution
+function arrayChunk(arr, n) {
+  const result = [];
+  for (let el of arr) {
+    const last = result[result.length - 1];
+    if (!last || last.length === n) {
+      result.push([el]);
+    } else {
+      last.push(el);
+    }
+  }
+  return result;
 }
 
 arrayChunk([1,2,3,4, 5], 3)
