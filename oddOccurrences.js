a717e3b2683945553fbea4de9cd49e53dd25c028
scirocco21 <sebgertz@gmail.com>
2019-07-03 22:31:41 -0400

    add codility problem oddOccurrences (66% solution - slow with O(2n) )

---
+++ oddOccurrences.js
@@ -0,0 +1,42 @@
+// A non-empty array A consisting of N integers is given. The array contains an odd number of elements, and each element of the array can be paired with another element that has the same value, except for one element that is left unpaired.
+
+// For example, in array A such that:
+
+//   A[0] = 9  A[1] = 3  A[2] = 9
+//   A[3] = 3  A[4] = 9  A[5] = 7
+//   A[6] = 9
+// the elements at indexes 0 and 2 have value 9,
+// the elements at indexes 1 and 3 have value 3,
+// the elements at indexes 4 and 6 have value 9,
+// the element at index 5 has value 7 and is unpaired.
+// Write a function:
+
+// function solution(A);
+
+// that, given an array A consisting of N integers fulfilling the above conditions, returns the value of the unpaired element.
+
+// For example, given array A such that:
+
+//   A[0] = 9  A[1] = 3  A[2] = 9
+//   A[3] = 3  A[4] = 9  A[5] = 7
+//   A[6] = 9
+// the function should return 7, as explained in the example above.
+
+function solution(A) {
+  let numberMap = {};
+  for (let num of A) {
+    if (numberMap[num]) {
+      numberMap[num] += 1;
+    } else {
+      numberMap[num] = 1;
+    }
+  }
+
+  for (let key in numberMap) {
+    if (numberMap[key] === 1) {
+      return parseInt(key)
+    }
+  }
+}
+
+// solution([1,2,1,2,3])
