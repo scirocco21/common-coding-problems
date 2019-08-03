3cba8534a9dd25485ba950d203ec7f5c77026532
scirocco21 <sebgertz@gmail.com>
2019-08-02 22:57:15 -0400

    simple stack implementation

---
+++ implementStack.js
@@ -0,0 +1,19 @@
+class Stack {
+  constructor() {
+    this.data = [];
+  }
+  remove = () => {
+    if (this.isEmpty()) {
+      return "Underflow";
+    }
+    return this.data.pop()
+  };
+  add = (el) => {
+    this.data.push(el);
+  }
+  isEmpty = () => {
+    this.data.length === 0 ? true : false
+  }
+}
+
+const stack = new Stack();
