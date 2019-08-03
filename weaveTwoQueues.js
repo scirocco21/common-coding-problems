9df0cfb51689217cfda47d90c917c966a01f5a11
scirocco21 <sebgertz@gmail.com>
2019-08-03 01:11:04 -0400

    refactor weave function: call isEmpty() consistently, return queue object rather than array

--- weaveTwoQueues.js
+++ weaveTwoQueues.js
@@ -1,21 +1,30 @@
 // Instructions: weave together two queues into a third one and return it. The two input queues may be of different lengths.
+
+// =======
+// SETUP
+// ======
 const Queue = require('./implementQueue');
 // create two queues
 const queue1 = new Queue();
 const queue2 = new Queue();
-// set up empty data store
-const newData = [];
 // add some dummy values
 queue1.add(1);
 queue1.add(3);
 queue2.add(2);
-// as long as the two queues are not empty, push items from queue into store
-while (!(queue2.isEmpty() && queue1.isEmpty())) {
-  if (queue1.peek()) {
-    newData.push(queue1.remove());
+// =========
+// MAIN FUNCTION
+// =========
+const weave = (q1,q2) => {
+  const resultQueue = new Queue();
+  // as long as the two queues are not empty, push items from queue into store
+  while (!(q1.isEmpty() && q2.isEmpty())) {
+    if (!q1.isEmpty()) {
+      resultQueue.add(q1.remove());
+    }
+    if (!q2.isEmpty()) {
+      resultQueue.add(q2.remove());
+    }
   }
-  if (queue2.peek()) {
-    newData.push(queue2.remove());
-  }
+  return resultQueue;
 }
-// expect newData to contain [1,2,3]
+// expect weave(queue1, queue2) to return [1,2,3]
