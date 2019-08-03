38f8c6bd39d39c9f560c4ea057e2718b67126ac5
scirocco21 <sebgertz@gmail.com>
2019-08-03 14:46:38 -0400

    add get size function to LL class

--- implementLinkedList.js
+++ implementLinkedList.js
@@ -1,3 +1,5 @@
+const clonedeep = require('lodash.clonedeep')
+
 class Node {
   constructor(data, next = null) {
     this.data = data;
@@ -12,5 +14,15 @@ class LinkedList {
   insertFirst(data) {
     // set next property of new head node to previous head node
     this.head = new Node(data, this.head);
+  }
+  size() {
+    let size = 0;
+    // don't overwrite head property of original list!
+    let linkedList = clonedeep(this)
+    while(linkedList.head) {
+      size++;
+      linkedList.head = linkedList.head.next;
+    }
+    return size;
   }
 }
