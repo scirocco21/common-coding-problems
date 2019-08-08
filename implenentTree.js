7e860078a66cc4ba62cce523733357cb82022a69
scirocco21 <sebgertz@gmail.com>
2019-08-08 14:41:59 -0400

    add tree Class with constructor and BF traversal

--- implenentTree.js
+++ implenentTree.js
@@ -10,3 +10,23 @@ class Node {
     this.children = this.children.filter(child => child.data !== data)
   }
 }
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+  traverseBF(fn) {
+    let nodes = [];
+    if (this.root) {
+      nodes.push(this.root);
+    }
+    while(nodes.length > 0) {
+      let node = nodes.shift();
+      fn(node);
+      for (let child of node.children) {
+        nodes.push(child);
+      }
+    }
+    return this;
+  }
+}
