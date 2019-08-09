ccdac10411a5cdbf29ccee849c1f6e65db2cd223
scirocco21 <sebgertz@gmail.com>
2019-08-09 09:07:34 -0400

    add comments to find tree width problem

--- printTreeWidth.js
+++ printTreeWidth.js
@@ -1,19 +1,26 @@
 // Given a root node, print the width of each level of the tree
 
+// 1. The value of "null" acts as stopper value to separate different levels of the tree
+// 2. When "null" is encountered, add a new counter to the counters array and add null back to the end of the nodes array
+
 function levelWidth(root) {
   const arr = [root, null];
-  const widths = [0];
+  const widthCounters = [0];
+
+  // iterate as long as array has elements
   while (arr.length) {
     const el = arr.shift();
+    // if you see the stop character, check if there is anything left in array
     if (el == null) { 
       if (arr.length) {
-        widths.push(0);
+        widthCounters.push(0);
         arr.push(null)
       } else {
-        return widths;
+        return widthCounters;
       }
+    // otherwise keep incrementing the counter for the relevant level and add child nodes
     } else {
-      widths[widths.length -1 ] += 1
+      widthCounters[widthCounters.length -1 ] += 1
       arr.push(...el.children);
     }
   }
