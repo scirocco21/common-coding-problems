06ad13e01c5c84e7b0dc00e48f56118a793be4b8
scirocco21 <sebgertz@gmail.com>
2020-06-27 23:41:27 -0400

    add O(n) set based solution

--- interview-cake/permutation-palindrom.js
+++ interview-cake/permutation-palindrom.js
@@ -2,7 +2,9 @@
 Write an efficient function that checks whether any permutation of an input string is a palindrome.
 */
 
-function permutationPalindrom(string) {
+/*A. O(n * logn) solution
+
+function permutationPalindrome(string) {
   let sorted = string.split("").sort();
   // isPalindrome tracks the number of times two characters differ from each other. If at the function execution, this value drops below zero (there is more than one unmatched character in the string), the string cannot be changed into a palindrome
   let isPalindrome = 0;
@@ -23,5 +25,22 @@ function permutationPalindrom(string) {
   }
   return true
 }
+*/
 
-permutationPalindrom("ivcc")
+// B. O(n) solution
+function permutationPalindrome(string) {
+  let seen = new Set();
+  for (let char of string) {
+    if (!seen.has(char)) {
+      seen.add(char)
+    } else {
+      seen.delete(char);
+    }
+  }
+  if (seen.size > 1) {
+    return false;
+  }  
+  return true;
+}
+
+permutationPalindrome("ivicc")
